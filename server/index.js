c323329daf09bd1755de6e9ec9ccd53e31902572
CHARLES ANTHONY BUCHANAN SR <charlesbuchanan911@gmail.com>
2025-11-12 23:52:13 -0500

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Crypto Oracle Server Running'));
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
